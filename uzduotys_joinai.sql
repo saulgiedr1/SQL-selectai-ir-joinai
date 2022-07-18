3cf837ad165d3968a6aae51fe45a2c6e761bfd19
saulgiedr1 <saulgiedr@gmail.com>
2022-07-18 17:50:32 +0300

    SELECT ir JOIN

---
+++ uzduotys_joinai.sql
@@ -0,0 +1,123 @@
+--Grąžinkite pirmo recepto visų ingredientų id ir pavadinimus.
+
+SELECT i.id, i.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id=sri.receptas_id
+JOIN ingridientai i ON i.id=sri.ingridientas_id
+WHERE r.id=1;
+
+--Grąžinkite antro recepto ingredientų kainas ir kalorijas per 100 g
+
+SELECT i.kaina, i.kalorijos_per100g
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id = sri.receptas_id
+JOIN ingridientai i ON i.id = sri.ingridientas_id
+WHERE r.id=2;
+
+--Grąžinkite trečio recepto ingredientų kainų sumą (nenaudojant kaina stulpelio recepto lentelėje, o sumuojant ingredientų kainas)
+
+SELECT SUM(i.kaina)
+FROM ingridientai i
+JOIN sujungimas_receptai_ingridientai sri ON i.id = sri.ingridientas_id
+JOIN receptai r ON r.id = sri.receptas_id
+WHERE r.id=3;
+
+SELECT SUM(i.kaina)
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id = sri.receptas_id
+JOIN ingridientai i ON i.id = sri.ingridientas_id
+WHERE r.id=3;
+
+--Grąžinkite ketvirto recepto visas alergijas ir jų id.
+
+SELECT a.id, a.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_alergijos sra ON r.id = sra.receptas_id
+JOIN alergijos a ON a.id = sra.alergija_id
+WHERE r.id = 4;
+
+--Grąžinkite visas alergijas receptų, kurių kaina yra mažiau nei 7.20
+
+SELECT a.id, a.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_alergijos sra ON r.id = sra.receptas_id
+JOIN alergijos a ON a.id = sra.alergija_id
+WHERE r.kaina < 7.20;
+
+--Grąžinkite kokie ingredientai yra naudojami pirmame, antrame ir trečiame recepte, tačiau pasikartojančių ingredientų neišveskite antrą kartą (tam naudojamas SELECT DISTINCT)
+
+SELECT DISTINCT  i.id, i.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id=sri.receptas_id
+JOIN ingridientai i ON i.id=sri.ingridientas_id
+WHERE r.id=1 OR r.id=2 OR r.id=3;
+
+--Grąžinkite receptų id, pavadinimą ir nurodymus, kurie yra “Universali” virtuvės receptai
+
+SELECT r.id, r.pavadinimas, r.nurodymai
+FROM receptai r
+JOIN sujungimas_receptai_kategorija srk ON r.id = srk.receptas_id
+JOIN kategorija_virtuve v ON v.id=srk.kategorija_id
+WHERE v.pavadinimas="Universali";
+
+--Grąžinkite visas devinto (id) ingrediento alergijas (id ir pavadinimas stulpelius) surikiuotus abėcėlės didėjančia tvarka pagal pavadinimo stulpelį.
+
+SELECT a.id, a.pavadinimas
+FROM ingridientai i
+JOIN sujungimas_ingridientai_alergijos sia ON i.id=sia.ingridientas_id
+JOIN alergijos a ON a.id=sia.alergija_id
+WHERE i.id=9
+ORDER BY a.pavadinimas
+
+--Grąžinkite visų alergijų id ir pavadinimą, kurių pavadinimas prasideda “s” raide (JOIN šiai užduočiai nereikia, reikia naudoti MySQL LIKE 
+
+SELECT a.id, a.pavadinimas 
+FROM alergijos a
+WHERE a.pavadinimas LIKE 's%';
+
+--Grąžinkite visų alergijų id, kurių pavadinimas baigiasi “ai” raidėmis 
+
+SELECT a.id, a.pavadinimas 
+FROM alergijos a
+WHERE a.pavadinimas LIKE '%ai';
+
+--Grąžinkite visus receptus, kurie priklauso arba “Lietuviška” arba “Morokietiška” arba “Vidurio Rytų” virtuvėms/šalims
+
+SELECT r.id, r.pavadinimas, v.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_kategorija srk ON r.id = srk.receptas_id
+JOIN kategorija_virtuve v ON v.id=srk.kategorija_id
+WHERE v.pavadinimas IN ('vidurio rytu','marokietiska','lietuviska');
+
+--Išveskite visų receptų kainas ir kalorijas, kuriuose yra pirmas (id) ingredientas
+
+SELECT r.kaina, r.kalorijos_per100g, i.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id = sri.receptas_id
+JOIN ingridientai i ON i.id = sri.ingridientas_id
+WHERE i.id=1;
+
+--Išveskite visus receptus, kuriuose yra “glitimas” alergija.
+
+SELECT r.pavadinimas
+FROM receptai r
+JOIN sujungimas_receptai_alergijos sra ON r.id = sra.receptas_id
+JOIN alergijos a ON a.id = sra.alergija_id
+WHERE a.pavadinimas = "glitimas";
+
+--Grąžinkite KELIUOSE ingredientuose yra alergija pavadinimu “glitimas”. (naudoti JOIN ir COUNT)
+
+SELECT COUNT(i.id)
+FROM ingridientai i
+JOIN sujungimas_ingridientai_alergijos sia ON i.id=sia.ingridientas_id
+JOIN alergijos a ON a.id=sia.alergija_id
+WHERE a.pavadinimas="glitimas";
+
+Grąžinkite KIEK yra ingredientų antrame recepte.
+
+SELECT COUNT( i.id)
+FROM receptai r
+JOIN sujungimas_receptai_ingridientai sri ON r.id=sri.receptas_id
+JOIN ingridientai i ON i.id=sri.ingridientas_id
+WHERE r.id=2;
+
---
+++ uzduotys_selectai.sql
@@ -0,0 +1,85 @@
+--ingridientu kainu stulpelis
+
+SELECT i.kaina
+FROM ingridientai i;
+
+--ingridientu kainu ir kaloriju stulpeliai
+
+SELECT i.kaina,i.kalorijos_per100g
+FROM ingridientai i
+WHERE i.kalorijos_per100g < 90;
+
+
+--ingridientu pavadinimo ir kainu stulpeliai
+
+SELECT i.pavadinimas,i.kaina
+FROM ingridientai i
+WHERE  i.kaina < 5.80 AND i.kaina > 2.20;
+
+--ingridientu stulpelis, kur id >=6
+
+SELECT i.pavadinimas , i.id
+FROM ingridientai i
+WHERE  i.id  >= 6;
+
+--didziausia kaina
+
+SELECT i.pavadinimas , MAX(kaina) 
+FROM ingridientai i;
+
+--receptu id ir pavadinimas
+
+SELECT r.id, r.pavadinimas 
+FROM receptai r;
+
+-- vidutine receptu kaina
+
+SELECT  AVG(kaina)
+FROM receptai r;
+
+----kainu suma
+SELECT  SUM(kaina)
+FROM receptai r;
+
+-- kuriu id nuo 3 iki 8 ir turi daugiau 500 kaloriju
+
+SELECT  r.id, r.pavadinimas , r.kalorijos_per100g
+FROM receptai r
+WHERE r.id >= 3 AND r.id <=8 AND r.kalorijos_per100g >500;
+
+--kuriu kaina < 10 ir kalorijos < 350
+
+SELECT  r.pavadinimas , r.nurodymai
+FROM receptai r
+WHERE r.kaina < 10  AND r.kalorijos_per100g < 350;
+
+-- vidutinis kaloriju sk , kuriu id <=5
+
+SELECT  AVG(kalorijos_per100g)
+FROM receptai r
+WHERE r.id <= 5;
+
+--brangiausio recepto pavadinimas 
+
+SELECT r.id, r.pavadinimas
+FROM receptai r
+WHERE kaina = (SELECT MAX(kaina)FROM receptai);
+
+--kaloringiausio recepto pavadinimas ir nurodymai
+
+SELECT  r.pavadinimas, r.nurodymai
+FROM receptai r
+WHERE kalorijos_per100g = (SELECT MAX(kalorijos_per100g)FROM receptai);
+
+-- brangiausiu receptu top 3
+SELECT   r.kaina
+FROM receptai r
+ORDER BY  r.kaina DESC
+LIMIT 3;
+
+--pigiausiu  5 receptu id ir pavadinimai
+SELECT  r.id, r.pavadinimas
+FROM receptai r
+ORDER BY  r.kaina
+LIMIT 5;
+
